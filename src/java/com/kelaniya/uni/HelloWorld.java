ff6bb0b1a078db86657c34c27e40611800bfd19c
Isuruni Rathnayaka <62780506+Isuruni@users.noreply.github.com>
2021-08-04 13:31:56 -0700

    Calculator Initial Commit

--- src/java/com/kelaniya/uni/HelloWorld.java
+++ src/java/com/kelaniya/uni/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.kelaniya.uni;
 
+import java.io.FileNotFoundException;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Calculator cal= new Calculator();//Creating an object from Calculator class
+        cal.user_input();
+        cal.calculating();
     }
 }
+
