f2f3d093e69d8689d09ebc27d092ac15254b66d0
Isuruni Rathnayaka <62780506+Isuruni@users.noreply.github.com>
2021-08-22 08:27:50 -0700

    updated

--- src/java/com/kelaniya/uni/v2/Main.java
+++ src/java/com/kelaniya/uni/v2/Main.java
@@ -3,9 +3,10 @@ package com.kelaniya.uni.v2;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws IOException {
 
         //Make sure to validate arguments before using
         if (args.length==0){
@@ -20,12 +21,33 @@ public class Main {
             System.out.println("Please enter add/su/mul/div");
             return;
         }
-        System.out.println(args[0]);
 
-        try {
-            Files.readAllLines(Paths.get("D:\\Dinu\\LEVEL 2\\SEM 1\\SENG 21222 - Software Construction\\Calculator\\numbers.txt~e0b470947ae7844d253813755abcff04449578ff"));
-        } catch (IOException e) {
-            e.printStackTrace();
+        //read from numbers.txt file
+        List<String> numberString=Files.readAllLines(
+                Paths.get("D:\\Dinu\\LEVEL 2\\SEM 1\\SENG 21222 - Software Construction\\Calculator\\numbers.txt~e0b470947ae7844d253813755abcff04449578ff")
+        );
+
+        double num1= Double.parseDouble(numberString.get(0));
+        double num2= Double.parseDouble(numberString.get(1));
+        System.out.println(num1);
+        System.out.println(num2);
+
+        double result= 0;
+
+        switch(operator){
+            case "add":
+                result=num1+num2;
+                break;
+            case "sub":
+                result=num1-num2;
+                break;
+            case "mul":
+                result=num1*num2;
+                break;
+            case "div":
+                result=num1/num2;
+                break;
         }
+        System.out.println(result);
     }
 }
